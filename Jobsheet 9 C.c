4524eb854dfbc9a29484aa7b5d307a0c5733e205
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-15 00:22:37 -0700

    Add files via upload
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ Jobsheet 9 C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	printf("Nama: Aufa dhiya ulhak");
+	printf("\nNim: F1B019029");
+	printf("\nKelompok: 3\n");
+int i;
+char nama[4]={'A','U','F','A'};
+char nama2[4]= "AUFA";
+printf("Array per Karakter = ");
+for(i=0;i<=4;i++)
+{
+printf("%c",nama[i]);
+}
+printf("\n"); printf("Array string =%s\n\n",nama2);
+return 0;
+}
