7eec92d3022c1571f6d53fe61bd6b52bee90b2eb
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-15 00:14:06 -0700

    Add files via upload
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Nama: Aufa dhiya ulhak";
+	cout<<"\nNim: F1B019029";
+	cout<<"\nKelompok: 3\n";
+int nim[7],i;
+for(i-0;i<=7;i++){
+cout<<"Masukkan Nilai "<<i+1<<" : ";
+cin>>nim[7];
+}
+}
