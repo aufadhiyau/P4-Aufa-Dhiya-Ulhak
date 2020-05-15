05d0403368874200ae44cbf7750083612bdd09ea
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-15 00:06:43 -0700

    Add files via upload
    Buatlah program pengurangan matriks dua dimensi dengan menggunakan NIM dua orang Mahasiswa

---
+++ Jobsheet 4 C.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	printf("Nama: Aufa dhiya ulhak");
+	printf("\nNim: F1B019029");
+	printf("\nKelompok: 3\n");
+	
+  int i, j, m, n, matriks1[2][2], matriks2[2][2], hasil[2][2];
+  printf("\nMasukkan jumlah baris matriks: ");
+  scanf("%d", &m);
+  printf("Masukkan jumlah kolom matriks: ");
+  scanf("%d", &n);
+  printf("Masukkan elemen matriks pertama: \n");
+  for (i = 0; i < m; i++){
+    for (j = 0; j < n; j++){
+      scanf("%d", &matriks1[i][j]);
+    }
+  }
+  printf("Masukkan elemen matriks kedua: \n");
+  for (i = 0; i < m; i++){
+    for (j = 0; j < n; j++){
+      scanf("%d", &matriks2[i][j]);
+    }
+  }
+  printf("Hasil pengurangan matriks: \n");
+  for (i = 0; i < m; i++){
+    for (j = 0; j < n; j++){
+      hasil[i][j] = matriks1[i][j] - matriks2[i][j];
+      printf("%d \t", hasil[i][j]);
+    }
+    printf("\n");
+  }
+}
