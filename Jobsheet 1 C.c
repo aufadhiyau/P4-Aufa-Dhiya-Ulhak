2b53d99144e3a08fb4806e6f45b65d946ca4892f
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-14 23:27:03 -0700

    Add files via upload
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main(){
+	int my_array[9]= { 1,2,3,4,5,6,7,8,9};
+	
+	printf("Nama: Aufa dhiya ulhak");
+	printf("\nNim: F1B019029");
+	printf("\nKelompok: 3\n");
+		
+	printf("\nData ke-1= %d",my_array[0]);
+	printf("\nData ke-2= %d",my_array[1]);
+	printf("\nData ke-3= %d",my_array[2]);
+	printf("\nData ke-4= %d",my_array[3]);
+	printf("\nData ke-5= %d",my_array[4]);
+	printf("\nData ke-6= %d",my_array[5]);
+	printf("\nData ke-7= %d",my_array[6]);
+	printf("\nData ke-8= %d",my_array[7]);
+	printf("\nData ke-9= %d",my_array[8]);
+}
