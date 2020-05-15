a540e5789539434b48d313a861eff42722db25ca
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-15 00:23:22 -0700

    Add files via upload
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Nama: Aufa dhiya ulhak";
+	cout<<"\nNim: F1B019029";
+	cout<<"\nKelompok: 3\n";
+int i;
+char nama[5]={'A','U','F','A','0'};
+char nama2[5]="AUFA";
+cout<<"Array per karakter =";
+for(i-0;i<=5;i++){
+cout<<nama[i];
+}
+cout<<endl;
+cout<<"Array string = "<<nama2<<endl;
+}
