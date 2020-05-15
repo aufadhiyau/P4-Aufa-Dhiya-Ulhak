29a188a82a2d9af5b20f320271a54bfb1943dea9
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-15 00:01:06 -0700

    Add files via upload
    Buatlah program penjumlahan matriks dua dimensi dengan menggunakan NIM dua orang Mahasiswa

---
+++ Jobsheet 3 C++.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int i, x;
+int matriks_1[2][2], matriks_2[2][2], matriks_3[2][2];
+char ulang;
+int main ()
+{
+	cout<<"Nama: Aufa dhiya ulhak";
+	cout<<"\nNim: F1B019029";
+	cout<<"\nKelompok: 3\n";
+ cout << "\nPenjumlahan Matriks\n";
+  cout << "\nData matriks 1 \n";
+  for (i = 0; i < 2; i++)
+  {
+   for (x = 0; x < 2; x++)
+   {
+    cout << "Masukan baris ke " << i+1 << " kolom ke " << x+1 << " : ";
+    cin >> matriks_1[i][x];
+   }
+  } 
+  cout << "\nData matriks 2 \n";
+  for (i = 0; i < 2; i++)
+  {
+   for (x = 0; x < 2; x++)
+   {
+    cout << "Masukan baris ke "<< i+1 <<" kolom ke "<< x+1 <<" : ";
+    cin >> matriks_2[i][x];
+   }
+  }
+  cout << "\nMatriks 1 + Matriks 2 : " << endl << endl;
+  for (i = 0; i < 2; i++)
+  {
+   for (x = 0; x < 2; x++)
+   {
+    matriks_3[i][x] = matriks_1[i][x] + matriks_2[i][x];
+    cout << "\t" << matriks_3[i][x];
+   }
+   cout << endl;
+  }
+}
