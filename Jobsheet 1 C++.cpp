044f0570526f99e16a75c637e9f2e11d018e38d1
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-14 23:28:05 -0700

    Add files via upload
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama: Aufa dhiya ulhak";
+	cout<<"\nNim: F1B019029";
+	cout<<"\nKelompok: 3\n";
+int data[9]= {1,13,2,8,4,6,12,15,19};
+	
+	cout << "\nData ke-1 adalah " << data[0] << endl;
+	cout << "Data ke-2 adalah " << data[1] << endl;
+	cout << "Data ke-3 adalah " << data[2] << endl;
+	cout << "Data ke-4 adalah " << data[3] << endl;
+	cout << "Data ke-5 adalah " << data[4] << endl;
+	cout << "Data ke-6 adalah " << data[5] << endl;
+	cout << "Data ke-7 adalah " << data[6] << endl;
+	cout << "Data ke-8 adalah " << data[7] << endl;
+	cout << "Data ke-95 adalah " << data[8] << endl;
+
+}
