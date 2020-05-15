c41f94923421e223861f236e04768453d337c661
aufadhiyau <64615944+aufadhiyau@users.noreply.github.com>
2020-05-14 23:29:34 -0700

    Add files via upload
    Buatlah program mengisi dan menampilkan elemen array denan index array sesuai dengan NIM terakhir setiap Mahasiswa

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+main()
+{
+	printf("Nama: Aufa dhiya ulhak");
+	printf("\nNim: F1B019029");
+	printf("\nKelompok: 3\n");
+	
+	int x[9];
+	printf("Masukkan nilai ke-1: ");
+	scanf("%i",&x[0]);
+	printf("Masukkan nilai ke-2: ");
+	scanf("%i",&x[1]);
+	printf("Masukkan nilai ke-3: ");
+	scanf("%i",&x[2]);
+	printf("Masukkan nilai ke-4: ");
+	scanf("%i",&x[3]);
+	printf("Masukkan nilai ke-5: ");
+	scanf("%i",&x[4]);
+	printf("Masukkan nilai ke-6: ");
+	scanf("%i",&x[5]);
+	printf("Masukkan nilai ke-7: ");
+	scanf("%i",&x[6]);
+	printf("Masukkan nilai ke-8: ");
+	scanf("%i",&x[7]);
+	printf("Masukkan nilai ke-9: ");
+	scanf("%i",&x[8]);
+	
+	printf("\nNilai ke-1= %i",x[0]);
+	printf("\nNilai ke-2= %i",x[1]);
+	printf("\nNilai ke-3= %i",x[2]);
+	printf("\nNilai ke-4= %i",x[3]);
+	printf("\nNilai ke-5= %i",x[4]);
+	printf("\nNilai ke-6= %i",x[5]);
+	printf("\nNilai ke-7= %i",x[6]);
+	printf("\nNilai ke-8= %i",x[7]);
+	printf("\nNilai ke-9= %i",x[8]);
+
+}
